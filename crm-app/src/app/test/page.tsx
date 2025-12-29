60ba6eb052d9675c5f391e3fd752cf273b5f8998
TWENTY2CRM <22geder@gmail.com>
2025-12-29 12:03:03 +0200

    Add test page

---
+++ crm-app/src/app/test/page.tsx
@@ -0,0 +1,9 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>✅ Test Page Works!</h1>
+      <p>If you see this, the app is running correctly.</p>
+      <a href="/login">Go to Login</a>
+    </div>
+  )
+}
